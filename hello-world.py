217d7df6ada9ec0ab0427976e147853f7fbf1c47
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-12 19:25:57 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st. set_page_config(layout="wide")
+st. set_page_config(layout="wide") # https://discuss.streamlit.io/t/how-to-increase-the-width-of-web-page/7697
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 st.markdown( # https://stackoverflow.com/questions/74611608/how-to-change-the-height-of-streamlit-sidebar
     """
@@ -13,10 +13,6 @@ st.markdown( # https://stackoverflow.com/questions/74611608/how-to-change-the-he
     """,
     unsafe_allow_html=True,
 )
-st.markdown( # Breite des main divs
-    """<style>.block-container {padding-bottom: 0.01rem;: 0 !important; padding-top: 0.5rem;: 0 !important;  \
-        padding-left: 0.25rem;: 0 !important; padding-right: 0.25rem;: 0 !important;}</style>""",
-    unsafe_allow_html=True,)
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 st.sidebar.title('Hello, World!')
