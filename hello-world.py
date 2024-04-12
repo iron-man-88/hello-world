49a05992a797b7b35c113851e5a329c721e5b22b
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-12 21:49:01 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -20,12 +20,12 @@ st.markdown( # Breite des main divs
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 st.sidebar.title('Hello, World!')
-st.title('Hello, Jesus Christ!')
+st.title('Hello Jesus Christ!')
 st.title('You are great!')
 st.title('The Preeminent One')
 
 # Create a two-column layout
-col1, col2 = st.columns([1, 2]) # this will just call methods directly in the returned objects
+col1, col2 = st.columns([5, 2]) # this will just call methods directly in the returned objects
 # Inside the first column, add the answer text
 with col1:
     st.write("")
