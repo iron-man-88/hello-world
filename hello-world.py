4bda4847b9cf0bab7ed36be22a15b33812ccfbd5
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-13 17:33:39 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,24 +1,7 @@
 import streamlit as st
 import pandas as pd
 st. set_page_config(layout="wide") # https://discuss.streamlit.io/t/how-to-increase-the-width-of-web-page/7697
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-st.markdown( # https://stackoverflow.com/questions/74611608/how-to-change-the-height-of-streamlit-sidebar
-    """
-    <style>
-    section[data-testid="stSidebar"][aria-expanded="true"]{height: 100% !important;}
-    section[data-testid="stSidebar"][aria-expanded="false"]{height: 100% !important;}              
-    div[class='appview-container st-emotion-cache-1wrcr25 ea3mdgi4']{margin-top: -60px;}
-    div[class='st-emotion-cache-6qob1r eczjsme3']{margin-top: -80px;}
-    div[class='st-emotion-cache-1nm2qww eczjsme2']{margin-top: 70px;}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-st.markdown( # Breite des main divs
-    """<style>.block-container {padding-bottom: 0.01rem;: 0 !important; padding-top: 0.5rem;: 0 !important;  \
-        padding-left: 0.25rem;: 0 !important; padding-right: 0.25rem;: 0 !important;}</style>""",
-    unsafe_allow_html=True,)
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 
 st.sidebar.title('Hello, World! It is wonderful')
 st.title('Hello Jesus Christ!')
