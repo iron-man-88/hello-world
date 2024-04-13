6e72e98266c87f3ebc64e8a3b4b16f1b293c9f5c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-13 19:35:03 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -42,7 +42,9 @@ def load_bundle(locale):
     df = pd.read_csv("database/text_bundle.csv")
     st.write("df ", df)
     df = df.query(f"locale == '{locale}'")# Create and return a dictionary of key/values.
+    st.write("df.query ", df)
     lang_dict = {df.key.to_list()[i]:df.value.to_list()[i] for i in range(len(df.key.to_list()))}
+    st.write("lang_dict ", lang_dict)
     return lang_dict
 def main():
     lang_options = {
