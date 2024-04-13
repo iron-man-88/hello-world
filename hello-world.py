20232bef003f4eeb8bc678bce4c28464588f182b
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-13 12:05:52 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -40,7 +40,7 @@ with col2:
 # https://medium.com/@groxli/konnichiwa-streamlit-689e6e48bdcb
 def load_bundle(locale):
     # Load in the text bundle and filter by language locale.
-    df = pd.read_csv("text_bundle.csv")
+    df = pd.read_csv("database/text_bundle.csv")
     df = df.query(f"locale == '{locale}'")# Create and return a dictionary of key/values.
     lang_dict = {df.key.to_list()[i]:df.value.to_list()[i] for i in range(len(df.key.to_list()))}
     return lang_dictdef main():
