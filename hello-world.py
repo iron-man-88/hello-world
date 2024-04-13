0aa57384e17f057a651a85f75231272d12098d15
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-13 14:59:56 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -37,20 +37,12 @@ with col2:
 
 
 
-# https://medium.com/@groxli/konnichiwa-streamlit-689e6e48bdcb
-def load_bundle(locale):
-    # Load in the text bundle and filter by language locale.
-    df = pd.read_csv("database/text_bundle.csv")
-    df = df.query(f"locale == '{locale}'")# Create and return a dictionary of key/values.
-    lang_dict = {df.key.to_list()[i]:df.value.to_list()[i] for i in range(len(df.key.to_list()))}
-    return lang_dictdef main():
-    lang_options = {
-        "English (US)":"en_US",
-        "日本語":"ja_JP"
-    }locale = st.radio(label='Language', options=list(lang_options.keys())) # Note we use the selected human-readable locale to get the relevant
-    # ISO locale code from the lang_options dictionary.
-    lang_dict = load_bundle(lang_options[locale])
-    st.subheader(lang_dict['greeting'])
+
+
+
+def main():
+    st.subheader("Hello, world.")
     returnif __name__ == "__main__":
     main()
+
 
