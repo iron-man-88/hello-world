5e42613ec0e282e51c4d035bfb6c95fd6713cb91
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-13 15:19:06 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -45,6 +45,22 @@ def load_bundle(locale):
     lang_dict = {df.key.to_list()[i]:df.value.to_list()[i] for i in range(len(df.key.to_list()))}
     st.write(lang_dict)
     return lang_dict
+def main():
+    lang_options = {
+        "English (US)":"en_US",
+        "日本語":"ja_JP"
+    }
+    locale = st.radio(label='Language', options=list(lang_options.keys())) # Note we use the selected human-readable locale to get the relevant
+    # ISO locale code from the lang_options dictionary.
+    lang_dict = load_bundle(lang_options[locale])
+    st.subheader(lang_dict['greeting'])
+#    returnif __name__ == "__main__":
+     main()
+#main()
 
+def mmain():
+    st.subheader("Hello, world.")
+    
+mmain()
 
 
