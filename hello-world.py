836975dab45afb57f0d887ffa83b01399d1f759c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-14 16:40:02 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -42,8 +42,9 @@ def FB_column(matrix, i):                 # Diese Funktion dient der Extraktion 
 
 df_all = pd.read_csv("database/text_bundle.csv")
 st.write("44 df_all ", df_all)
-FullListColumn2 = FB_column(df_all, 1) # Spalte "1" wird extrahiert
-st.write("46 FullListColumn2 ", FullListColumn2)
+#FullListColumn2 = FB_column(df_all, 1) # Spalte "1" wird extrahiert
+df_dee_index0 = df_all.index[1] # selber
+st.write("46 df_dee_index0 ", df_dee_index0)
 
 # https://medium.com/@groxli/konnichiwa-streamlit-689e6e48bdcb
 @st.cache_data
