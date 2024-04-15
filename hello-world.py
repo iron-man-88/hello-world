2ea1dd5a8c009210fd7f90dd1bc29de604246681
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-15 21:17:29 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -50,6 +50,9 @@ FullListColumn2 = FB_column(FullList, 4)
 st.write("50 FullListColumn2 ", FullListColumn2)
 IndexSeite = FullListColumn2.index("Hallo, Welt1!")
 st.write("52 IndexSeite ", IndexSeite)
+FullList['value',IndexSeite].tolist()
+st.write("54 FullList ", FullList)
+
 vvaluee = FullList[1,1]
 st.write("54 vvaluee ", vvaluee)
 
