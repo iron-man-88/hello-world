7360784e4445c706fc4831cf060e558b5d9005d4
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-15 21:36:40 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -73,7 +73,7 @@ else:
     df_all_column = 6
 
 sel_value = df_all.iloc[0,df_all_column]
-st.write("59 sel_value ", sel_value)
+st.write("76 sel_value ", sel_value)
 
 # https://medium.com/@groxli/konnichiwa-streamlit-689e6e48bdcb
 @st.cache_data
