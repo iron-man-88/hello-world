47295d8e641949a464589c32fdc3913fabd8c37e
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-16 10:07:49 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -34,6 +34,7 @@ with col1:
 # Inside the second column, add the image
 with col2:
 #    st.image("img/001.jpg", caption="Lightning Image", use_column_width=True)
+    st.write("")
 ###################################################################################################################
 @st.cache_data
 def FB_column(matrix, i):                 # Diese Funktion dient der Extraktion einzelner Spalten aus einer Liste
