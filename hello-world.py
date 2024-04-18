5a29ed734708058d5967a7543035f3e7cbe86fb2
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-18 11:01:07 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -165,9 +165,8 @@ if 'rradio_click' not in st.session_state:
 
 bbutton_read = st.radio('select choice',options=['Deutsch','中文','English'],key='rrdkey',index=0,horizontal=True)
 st.session_state.bbut_click = 'y'
-if (st.session_state.bbut_click=='y') and (bbutton_read == bbutton_read):
-    st.session_state.rradio_click = bbutton_read
-    st.write("171 bbutton_read ", bbutton_read)
+st.session_state.rradio_click = bbutton_read
+st.write("171 bbutton_read ", bbutton_read)
 
 st.write("173 bbutton_read ", bbutton_read)
 
