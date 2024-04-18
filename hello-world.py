4a7c946f46946cfcc11470ff4a489d5f7b79865d
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-18 10:51:48 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -170,7 +170,7 @@ if st.session_state.bbut_click == 'y':
         st.session_state.rradio_click = bbutton_read
         st.write("171 bbutton_read ", bbutton_read)
 
-st.write("173 bbutton_read ", bbutton_read)
+#st.write("173 bbutton_read ", bbutton_read)
 
 
 
