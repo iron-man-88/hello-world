b43d8fe7838ccdb7b0d1101c53821fec5bb48378
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-18 09:29:42 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -129,7 +129,7 @@ def read_file_text():
     return df_read_file
 
 read_file_text()
-st.write("132 df_read_file.iloc[0,1]", df_read_file)
+st.write("132 df_read_file.iloc[0,1]", read_file_text().iloc[0,1])
 
 if 'but_click' not in st.session_state:
     st.session_state.but_click = ''
