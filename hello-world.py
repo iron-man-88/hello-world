b991ab54edd878becdd26023c9b754b63c409520
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-18 08:59:18 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -122,13 +122,13 @@ from PIL import Image
 df_read_file = None
 @st.cache_data
 def read_file_text():
-    global df_read_file
+    #global df_read_file
     df_read_file = pd.read_csv("database/text_3_lang.csv")
     st.write("127 df_read_file ", df_read_file)
     return df_read_file
 
 read_file_text()
-st.write("130 df_read_file.iloc[0,1]", df_read_file.iloc[0,1])
+st.write("131 df_read_file.iloc[0,1]", df_read_file.iloc[0,1])
 
 if 'but_click' not in st.session_state:
     st.session_state.but_click = ''
