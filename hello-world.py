9df5a03e2d8f324972611fb293e00f072b02c98e
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-18 19:40:18 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -65,10 +65,12 @@ def read_file_text():
 
 read_file_text()
 df_single_value = read_file_text()
-st.write("68 df_single_value.iloc[0,df_all_column]", df_single_value.iloc[0,df_all_column])
 
+rrow_index = df_single_value.index[df_single_value['key'] == 'greeting_1'].values[0]  # test
+st.write("70 row_index ", row_index) # test
+st.write("71 df_single_value.iloc[0,df_all_column]", df_single_value.iloc[rrow_index,df_all_column])
 
-
+greeting_1 = df_single_value.iloc[rrow_index,df_all_column]
 st.write(greeting_1)
 ###################################################################################################################
 ###################################################################################################################
