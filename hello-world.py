78a52052563ba708bbd5174c545263c922df2cb8
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-19 15:09:55 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -66,8 +66,8 @@ def read_file_text():                                                  ## ## cre
 #####################################
 selected_Page_Texts = None
 selected_Page_Text = None
-selected_Page_Texts = df_read_file[df_read_file['page'] == 1, 'key']
-st.write("69 selected_Page_Texts" , selected_Page_Texts)
+#selected_Page_Texts = df_read_file[df_read_file['page'] == 1, 'key']
+#st.write("69 selected_Page_Texts" , selected_Page_Texts)
 selected_Page_Text = df.query("page == 1")
 st.write("72 selected_Page_Text" , selected_Page_Text)
 #####################################
