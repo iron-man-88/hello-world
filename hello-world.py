bc18fa04432cd66a6d26818d5e4a9faa46a98f03
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-19 16:27:41 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -52,6 +52,19 @@ elif button_read == "中文":
 else:
     df_lang_column = 3
 
+
+## ## create var "df_read_file = None" with value "None"
+df_read_file = None
+@st.cache_data
+def read_file_text():
+    global df_read_file 
+    df_read_file = pd.read_csv("database/text_3_lang.csv")
+    return df_read_file
+    st.write("63 df_read_file " , df_read_file)
+st.write("64 df_read_file " , df_read_file)
+
+
+
 #####################################https://stackoverflow.com/questions/73659180/how-to-stop-streamlit-to-reseting-after-using-radio###############################
 #from PIL import Image
 ## ## create var "df_read_file = None" with value "None", copy FuBa "read_file_text()" to "@st.cache_data"
@@ -62,16 +75,6 @@ else:
 #    df_read_file = pd.read_csv("database/text_3_lang.csv")             ## ## read csv data file, FuBa is necessary, otherwise "@st.cache_data" is not possible!!!
 #    return df_read_file                                                ## ## return is for selected output, otherwise value is empty    st.write("64 df_read_file " , df_read_file)
 #st.write("65 df_read_file " , df_read_file)
-
-## ## create var "df_read_file = None" with value "None"
-df_read_file = None
-@st.cache_data
-def read_file_text():
-    global df_read_file 
-    df_read_file = pd.read_csv("database/text_3_lang.csv")
-    return df_read_file
-    st.write("73 df_read_file " , df_read_file)
-st.write("74 df_read_file " , df_read_file)
 
 #####################################https://stackoverflow.com/questions/73659180/how-to-stop-streamlit-to-reseting-after-using-radio###############################
 
