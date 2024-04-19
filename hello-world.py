696f3ee63f1de26ea08e5ed2624b2b1bbf10325d
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-19 13:35:11 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -45,7 +45,6 @@ if 'radio_click' not in st.session_state:
 
 button_read = st.radio('select choice',options=['Deutsch','中文','English'],key='rrdkey',index=0,horizontal=True)
 st.session_state.radio_click = button_read
-#st.write("48 button_read ", button_read)
 if button_read == "Deutsch":
     df_lang_column = 1
 elif button_read == "中文": 
@@ -59,9 +58,10 @@ else:
 df_read_file = None                                                    ## ## create var "df_read_file = None" with value "None"
 @st.cache_data                                                         ## ## copy FuBa "read_file_text()" to "@st.cache_data"
 def read_file_text():                                                  ## ## create FuBa "read_file_text()"
-    global df_read_file                                                ## ## 
-    df_read_file = pd.read_csv("database/text_3_lang.csv")
-    return df_read_file
+    global df_read_file                                                ## ## "global" is for global access of var "df_read_file" 
+    df_read_file = pd.read_csv("database/text_3_lang.csv")             ## ## read csv data file, FuBa is necessary, otherwise "@st.cache_data" is not possible!!!
+    return df_read_file                                                ## ## return is for selected output, otherwise value is empty
+#####################################https://stackoverflow.com/questions/73659180/how-to-stop-streamlit-to-reseting-after-using-radio###############################
 
 ############https://stackoverflow.com/questions/6181935/how-do-you-create-different-variable-names-while-in-a-loop#######################
 var_temp_list = []      
