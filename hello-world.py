2a15584a2e442943cdbb44d3d092b5dde4cd5ed6
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-19 16:30:04 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -53,8 +53,7 @@ else:
     df_lang_column = 3
 
 
-## ## create var "df_read_file = None" with value "None"
-df_read_file = None
+#df_read_file = None
 @st.cache_data
 def read_file_text():
     global df_read_file 
