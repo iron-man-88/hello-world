3a1cb54ef822fb20b3422cabd7695128a229db6c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-20 10:40:10 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import streamlit_extras as se
-from streamlit_extras.dataframe_explorer import dataframe_explorer
+#import streamlit_extras as se
+#from streamlit_extras.dataframe_explorer import dataframe_explorer
 st. set_page_config(layout="wide") # https://discuss.streamlit.io/t/how-to-increase-the-width-of-web-page/7697
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
