5da6e508ccda64e58ce0e273f4853cb3c83d9be9
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-20 11:22:34 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -277,6 +277,24 @@ st.dataframe(ddf, use_container_width=st.session_state.use_container_width)
 
 
 
-dfff = pd.read_clipboard()          # here, we are initializing the df variable to store the #copied data from the Excel file  
-dfff
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from io import StringIO
 
+copy_button = Button(label="Get Clipboard Data")
+copy_button.js_on_event("button_click", CustomJS(code="""
+    navigator.clipboard.readText().then(text => document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: text})))
+    """))
+result = streamlit_bokeh_events(
+    copy_button,
+    events="GET_TEXT",
+    key="get_text",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        dfdf = pd.read_csv(StringIO(result.get("GET_TEXT")))
+        st.table(dfdf)
