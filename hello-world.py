b044bb8991d91b1f008bab4ea30c20bcfbd44944
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-22 20:20:31 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -268,7 +268,7 @@ st.dataframe(ddf, use_container_width=st.session_state.use_container_width)
 
 
 import pyperclip
-text_to_be_copied = 'wwwwwwwwwwwwwwwwwwThe text to be copied to the clipboard.'
+text_to_be_copied = 'The text to be copied to the clipboard.'
 pyperclip.copy(text_to_be_copied)
 clipboard_content = pyperclip.paste()
 st.write("pyperclip.paste ", clipboard_content)
