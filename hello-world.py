51a5354f214db7b8e4ed915e4e9b882d26ce9248
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-22 21:52:28 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -265,9 +265,7 @@ st.dataframe(ddf, use_container_width=st.session_state.use_container_width)
 
 
 a=st.text_area('Type in the text_area and click copy')
-st.button('Copy'):
-    pyperclip.copy(a)
+if st.button('Copy'):
     st.success('Text copied successfully!')
-
 
 
