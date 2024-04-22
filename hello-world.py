ce86dff93174daaa41df301d0e233a0bf6d288bf
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-22 20:06:53 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -267,28 +267,9 @@ st.dataframe(ddf, use_container_width=st.session_state.use_container_width)
 
 
 
+import pyperclip
+#text_to_be_copied = 'wwwwwwwwwwwwwwwwwwThe text to be copied to the clipboard.'
+#pyperclip.copy("text_to_be_copied")
 
-
-
-
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-from io import StringIO
-
-copy_button = Button(label="Get Clipboard Data")
-copy_button.js_on_event("button_click", CustomJS(code="""
-    navigator.clipboard.readText().then(text => document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: text})))
-    """))
-result = streamlit_bokeh_events(
-    copy_button,
-    events="GET_TEXT",
-    key="get_text",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        dfdf = pd.read_csv(StringIO(result.get("GET_TEXT")))
-        st.table(dfdf)
+clipboard_content = pyperclip.paste()
+st.write("pyperclip.paste ", clipboard_content)
