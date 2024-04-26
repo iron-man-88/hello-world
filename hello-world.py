45f210d0e5a43e1038cdb5900f335b6cf86f6c25
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-26 15:16:08 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -404,16 +404,17 @@ cols[2].write(df.dtypes)
 st.write("404 ", result) #######################################################
 ddata = [['HSK 1', 'Adjektiv', '10', 'dom'],['HSK 1', 'Adjektiv', 12, 'dom'],
 ['HSK 1', 'Adjektiv', 7, 'dom']]
-dframe = pd.DataFrame(ddata,columns=['HSK', 'Wortart','age','name'])
+dframe = pd.DataFrame(ddata,columns=['HSK', 'Wortart','col_key','age','name'])
 hsks = ['HSK 1', 'HSK 2', 'HSK 3', 'HSK 4', 'HSK 5']
 wortarten = ['Adjektiv', 'Abverb', 'yellow', 'green', 'blue', 'indigo', 'violet']
 config = {
     'HSK' : st.column_config.SelectboxColumn('HSK', options=hsks),
     'Wortart' : st.column_config.SelectboxColumn('Wortart', options=wortarten),
+    'col_key' : st.column_config.TextColumn('Key_col', key=True),
     'age' : st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
     'name' : st.column_config.TextColumn('Name (required)', width='large', default="st.", required=True)
 }
-result = st.data_editor(dframe, column_config = config, num_rows='fixed', hide_index=False) #org
+result = st.data_editor(dframe, column_config = config, num_rows='dynamic', hide_index=False) #org
 if st.button('Get results'):
     st.write("422 ", result)
 #######################################################
