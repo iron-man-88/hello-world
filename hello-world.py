cc49090edd8cc49586a94c8fc698b5fd2a6a1a1a
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-04-27 20:08:18 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -505,20 +505,17 @@ st.write(st.session_state.test)
 #########
 checks = st.columns(4)
 with checks[0]:
-    st.checkbox('Flip the value', key='test1')
+    st.checkbox('Flip the value1', key='test1')
     st.write(st.session_state.test1)
 with checks[1]:
-    st.checkbox('Flip the value', key='test2')
+    st.checkbox('Flip the value2', key='test2')
     st.write(st.session_state.test2)
 with checks[2]:
-    st.checkbox('Flip the value', key='test3')
+    st.checkbox('Flip the value3', key='test3')
     st.write(st.session_state.test3)
 with checks[3]:
-    st.checkbox('Flip the value', key='test4')
+    st.checkbox('Flip the value4', key='test4')
     st.write(st.session_state.test4)
-#########
-st.checkbox('Flip the value', key='test')
-st.write(st.session_state.test)
 #########
 
 
