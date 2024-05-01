69e9a1f353a1cd869687474f1ecab5b2d63282a3
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-02 07:53:40 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -487,6 +487,44 @@ st.write("448 ")
 
 
 
+def options_select():
+    if "selected_options" in st.session_state:
+        if -1 in st.session_state["selected_options"]:
+            st.session_state["selected_options"] = [available_options[0]]
+            st.session_state["max_selections"] = 1
+        else:
+            st.session_state["max_selections"] = len(available_options)
+
+
+available_options = [i for i in range(-1, 10)]
+if "max_selections" not in st.session_state:
+    st.session_state["max_selections"] = len(available_options)
+
+st.multiselect(
+    label="Select an Option",
+    options=available_options,
+    key="selected_options",
+    max_selections=st.session_state["max_selections"],
+    on_change=options_select,
+    format_func=lambda x: "All" if x == -1 else f"Option {x}",
+)
+
+st.write(
+    available_options[1:]
+    if st.session_state["max_selections"] == 1
+    else st.session_state["selected_options"]
+)
+
+
+
+
+
+
+
+
+
+
+
 #file:///F:/uni/Faecher/Unterricht/2023/22-Konversation/Konversation/Konversation.html
 #PDF 3 Definite articles + indefinite articles.
 #3-nominakkusnegation_Ocean.pdf
