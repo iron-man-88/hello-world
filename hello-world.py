e42f7fa8c3179c6a2f5b808a48c8da0423fc175c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-11 22:47:45 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -76,3 +76,93 @@ def add_numbers(num1, num2):
 # function call with two values
 aaa = add_numbers(5, 4)
 st.write("78 Sum: ", aaa)
+
+
+
+### Mögliche Optionen zur Konfiguration des Data Frame Editors Beginn
+### https://discuss.streamlit.io/t/split-st-radio-in-columns/17044/3 ###
+col1_read_write = ["read", "write"] # Lese- oder Schreibrechte
+col2_use_container_width = [True, False] # use_container_width Ja oder Nein
+col3_hide_index = [True, False] # hide_index Ja oder Nein
+col4_column_order = [True, False] # Anzeige von Spalten oder und deren Reihenfolge column_order (Iterable of str or None)
+col5_options = ["aGPS", "aGRWG"]
+col6_options = ["aPLUG", "aRNG"]
+agree = st.checkbox('def')
+
+if "current" not in st.session_state:
+    st.session_state.current = col1_read_write[0]
+
+if "col1_old" and "col2_old" and "col3_old" and "col4_old" and "col5_old" and "col6_old" not in st.session_state:
+    st.session_state.col1_old = col1_read_write[0]
+    st.session_state.col2_old = col2_use_container_width[0]
+    st.session_state.col3_old = col3_hide_index[0]
+    st.session_state.col4_old = col4_column_order[0]
+    st.session_state.col5_old = col5_options[0]
+    st.session_state.col6_old = col6_options[0]
+
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+button_num_rows = col1.radio("read or write", col1_read_write, horizontal=True)
+button_use_container_width = col2.radio("container width", col2_use_container_width, horizontal=True)
+button_hide_index = col3.radio("index view", col3_hide_index, horizontal=True)
+button_column_order = col4.radio("order", col4_column_order, horizontal=True)    #del
+col5_choice = col5.radio("", col5_options, horizontal=True)
+col6_choice = col6.radio("", col6_options, horizontal=True)
+
+if button_num_rows != st.session_state.col1_old:
+    st.session_state.current = button_num_rows
+    st.session_state.col1_old = button_num_rows
+
+if button_use_container_width != st.session_state.col2_old:
+    st.session_state.current = button_use_container_width
+    st.session_state.col2_old = button_use_container_width
+
+if button_hide_index != st.session_state.col3_old:
+    st.session_state.current = button_hide_index
+    st.session_state.col3_old = button_hide_index
+
+if button_column_order != st.session_state.col4_old:
+    st.session_state.current = button_column_order
+    st.session_state.col4_old = button_column_order
+
+if col5_choice != st.session_state.col5_old:
+    st.session_state.current = col5_choice
+    st.session_state.col5_old = col5_choice
+
+if col6_choice != st.session_state.col6_old:
+    st.session_state.current = col6_choice
+    st.session_state.col6_old = col6_choice
+
+if st.session_state.current != None:     ## del
+    st.write("302 You've picked: ", st.session_state.current, button_num_rows)    ## del
+
+############# https://medium.com/streamlit/multi-select-all-option-in-streamlit-3c92a0f20526
+def options_select():
+    if "selected_options" in st.session_state:
+        if -1 in st.session_state["selected_options"]:
+            st.session_state["selected_options"] = [available_options[0]]
+            st.session_state["max_selections"] = 1
+        else:
+            st.session_state["max_selections"] = len(available_options)
+
+available_options = [-1, "HSK", "Wortart", "Häufigkeit", "Wort cn", "Wort py", "Wort de", "Wort cn", "Satz cn", "Satz py", "Satz de", "Satz en"]
+if "max_selections" not in st.session_state:
+    st.session_state["max_selections"] = len(available_options)
+
+st.multiselect(
+    label="Select an Option",
+    options=available_options,
+    key="selected_options",
+    max_selections=st.session_state["max_selections"],
+    on_change=options_select,
+    format_func=lambda x: "All" if x == -1 else f"Option {x}",
+)
+
+st.write(
+    available_options[1:]
+    if st.session_state["max_selections"] == 1
+    else st.session_state["selected_options"]
+)
+############# https://medium.com/streamlit/multi-select-all-option-in-streamlit-3c92a0f20526
+
+### Mögliche Optionen zur Konfiguration des Data Frame Editors Ende
