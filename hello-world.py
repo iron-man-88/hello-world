5dadbc54acf1e2bc975d46de483b59a2ade49355
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-11 21:25:51 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -33,11 +33,11 @@ st.title('The Preeminent One')
 if 'but_click' not in st.session_state:
     st.session_state.but_click = ''
 if 'radio_click' not in st.session_state:
-    st.session_state.radio_click = 'DDeutsch'
+    st.session_state.radio_click = 'Deutsch'
 
 button_read = st.radio('select choice',options=['DDeutsch','中文','English'],key='rrdkey',index=0,horizontal=True)
 st.session_state.radio_click = button_read
-if button_read == "DDeutsch":
+if button_read == "Deutsch":
     df_lang_column = 1
 elif button_read == "中文": 
     df_lang_column = 2
@@ -52,14 +52,14 @@ else:
 df_read_file_cell = None                                                    ## ## create var "df_read_file = None" with value "None"
 df_read_file      = None                                                    ## ## create var "df_read_file = None" with value "None"
 @st.cache_data                                                         ## ## copy FuBa "read_file_text()" to "@st.cache_data"
-def read_file_text():                                                  ## ## create FuBa "read_file_text()"
+def read_file_text(df_lang_column):                                    ## ## create FuBa "read_file_text()"
     global df_read_file_cell                                                ## ## "global" is for global access of var "df_read_file" 
     global df_read_file                                                ## ## "global" is for global access of var "df_read_file" 
     df_read_file = pd.read_csv("database/text_3_lang.csv")             ## ## read csv data file, FuBa is necessary, otherwise "@st.cache_data" is not possible!!!
     st.write("57 df_read_file ", df_read_file)
     row_index = df_read_file.index[df_read_file['key'] == 'greeting_2'].values[0]  # test
     st.write("60 row_index ", row_index) # test
-    df_read_file_cell = df_read_file.iloc[row_index][2]   #    df.iloc[index][col]
+    df_read_file_cell = df_read_file.iloc[row_index][df_lang_column]   #    df.iloc[index][col]
     st.write("63 df_read_file_cell ", df_read_file_cell) # test
     return df_read_file_cell, df_read_file                                                ## ## return is for selected output, otherwise value is empty    st.write("64 df_read_file " , df_read_file)
 
