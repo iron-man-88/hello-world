4c5c6172d27d0736f3cc97961dfcae75c3947ee5
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-11 22:21:33 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -51,21 +51,15 @@ else:
 
 @st.cache_data                                                         ## ## copy FuBa "read_file_text()" to "@st.cache_data"
 def read_file_text(df_lang_column):                                    ## ## create FuBa "read_file_text()"
-#    global df_read_file_cell                                                ## ## "global" is for global access of var "df_read_file" 
-#    global df_read_file                                                ## ## "global" is for global access of var "df_read_file" 
     df_read_file = pd.read_csv("database/text_3_lang.csv")             ## ## read csv data file, FuBa is necessary, otherwise "@st.cache_data" is not possible!!!
-    st.write("57 df_read_file ", df_read_file)
+#    st.write("57 df_read_file ", df_read_file)
     row_index = df_read_file.index[df_read_file['key'] == 'greeting_2'].values[0]  # test
-    st.write("60 row_index ", row_index) # test
+#    st.write("60 row_index ", row_index) # test
     df_read_file_cell = df_read_file.iloc[row_index][df_lang_column]   #    df.iloc[index][col]
-    st.write("63 df_read_file_cell ", df_read_file_cell) # test
+#    st.write("63 df_read_file_cell ", df_read_file_cell) # test
     return df_read_file_cell                                                ## ## return is for selected output, otherwise value is empty    st.write("64 df_read_file " , df_read_file)
 
-#df_read_file_cell = None                                                    ## ## create var "df_read_file = None" with value "None"
-#df_read_file      = None                                                    ## ## create var "df_read_file = None" with value "None"
-
 st.write("67 df_read_file_cell ",read_file_text(df_lang_column))
-#st.write("67 df_read_file_cell ", df_read_file_cell)
 #####################################https://stackoverflow.com/questions/73659180/how-to-stop-streamlit-to-reseting-after-using-radio###############################
 
 def greet(name, namee):
