2f9c2ddaf556b0f1e45e2e84a322335403a1038a
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-13 17:50:35 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -265,10 +265,10 @@ st.write("257 ")
 
 
 
-titleimg = “static/Picture7.png”
+titleimg = “img/AB_01_01.svg”
 def set_bg_hack(main_bg):
 # set bg name
-main_bg_ext = “png”
+main_bg_ext = “svg”
 st.markdown(
     f"""
      <style>
@@ -283,3 +283,4 @@ st.markdown(
      """,
     unsafe_allow_html=True,
 )
+set_bg_hack(titleimg)
