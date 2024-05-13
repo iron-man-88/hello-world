7bbc582ecb5c4a1866acb3c2aa5ce756e5e67b62
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-13 19:50:44 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -321,3 +321,34 @@ background-size: cover;
 '''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def sidebar_bg(side_bg):
+
+   side_bg_ext = 'png'
+
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+
+side_bg = 'AB_01_01.svg'
+sidebar_bg(side_bg)
