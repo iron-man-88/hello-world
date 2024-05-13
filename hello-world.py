cf802b5510c676401d696959346a3dff2aefdb0c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-13 19:40:23 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -311,3 +311,13 @@ set_png_as_page_bg('AB_01_01.svg')
 
 
 
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
