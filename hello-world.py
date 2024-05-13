83d37be589a016f741f9ced2cc9e560fbfde7b9c
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-13 22:16:09 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -270,8 +270,10 @@ st.write("257 ")
 
 
 with st.popover("Open popover"):
-    st.markdown("Hello World 👋"
-               position: relative)
+    st.markdown("Hello World 👋",
+               position: relative,
+               top: 80px,
+               left: 10px)
     name = st.text_input("What's your name?")
 
 st.write("Your name:", name)
