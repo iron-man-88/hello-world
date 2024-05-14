be582a1c55027aec08242012b14009ac876533f2
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-14 11:17:59 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -450,7 +450,9 @@ def example2():
             {
                 border: 1px solid rgba(49, 51, 63, 0.2);
                 border-radius: 0.5rem;
-                padding: calc(1em - 1px)
+                padding: calc(1em - 1px);
+                position: relative;
+                left: 100px;
             }
             """,
     ):
