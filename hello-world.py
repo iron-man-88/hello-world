cba4f8984bf005828b45c3f535aeb209d023cbc1
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-14 11:40:35 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -274,7 +274,7 @@ svg_url = "AB_01_01.svg"
 #my_png = cairosvg.svg2png(url=svg_url, output_width=426, output_height=240)
 st.image(svg_url)
 
-left = 30
+leftt = 30
 def example2(leftt):
     with stylable_container(
         key="green_button",
