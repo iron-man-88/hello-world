3c1342e61386c8fd439784bc546fe1f876105f9e
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-17 12:56:56 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -264,7 +264,7 @@ st.write("257 ")
 
 #https://discuss.streamlit.io/t/passing-variable-containing-text-to-markdown/16069/3
 variable_output = st.text_input("Enter some text", value="Streamlit is awesome")
-left_position = st.slider("Enter a left", 1, 340, value=0)
+left_position = st.slider("Left", 1, 1340, value=0)
 
 html_str = f"""
 <style>
