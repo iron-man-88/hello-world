7fb75ea1f95b417d31e13bab1e21e20c3ece1018
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-17 09:39:26 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -323,9 +323,9 @@ with stylable_container(
         """,
 ):
     st.markdown("This is a test_pop_over container with a border.")
-    with st.popover("Open popover"):
-        st.markdown("Hello World 👋")
-        name = st.text_input("What's your name?")
+        with st.popover("Open popover"):
+            st.markdown("Hello World 👋")
+            name = st.text_input("What's your name?")
 
 st.write("Your name:", name)
 ##########################
