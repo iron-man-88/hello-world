0373a648ea986c612546a145a99561ca2023a7a7
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-17 14:31:58 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -24,7 +24,7 @@ st.markdown( # https://stackoverflow.com/questions/74611608/how-to-change-the-he
 )
 st.markdown( # Breite des main divs
     """<style>.block-container {padding-bottom: 0.01rem;: 0 !important; padding-top: 0.5rem;: 0 !important;  \
-        padding-left: 0.25rem;: 0 !important; padding-right: 0.25rem;: 0 !important;}</style>""",
+        padding-left: 0.25rem;: 0 !important; padding-right: 0.25rem;: 0 !important; position: fixed !important}</style>""",
     unsafe_allow_html=True,)
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
