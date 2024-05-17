84ed299e0c5e610b638edc84e75dce7bbaff77ef
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-17 10:02:57 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -325,9 +325,9 @@ with stylable_container(
             """,
     ):
         po = st.popover(label='green popover')
-        nnname=po.text_input('name', key='nname')
+        po.text_input('name', key='nname')
 
-st.write("Your name:", nnname)
+st.write("Your name:", nname)
 
 ##########################
 
