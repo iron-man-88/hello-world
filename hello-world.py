2193a0c4e64e3fdb5fd8967f7b152f6fce60d2c9
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-17 13:53:40 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -521,7 +521,7 @@ if bg_color is None:
     bg_color = "white"
 
 # Generate CSS to target the floating footer container
-css = float_css_helper(bottom="0", background=bg_color)
+css = float_css_helper(top="0", background=bg_color)
 
 # Float the footer container and provide CSS to target it with
 footer_container.float(css)
