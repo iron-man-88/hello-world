4347d17670574769c95b4a7a8390f68cc497bab2
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-19 21:31:54 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -307,6 +307,31 @@ st.markdown(html_str, unsafe_allow_html=True)
 st.write("Your name:")
 
 
+#################################################
+def change_label_style(label, font_size='12px', font_color='blue', font_family='sans-serif'):
+    html = f"""
+    <script>
+        var elems = window.parent.document.querySelectorAll('p');
+        var elem = Array.from(elems).find(x => x.innerText == '{label}');
+        elem.style.fontSize = '{font_size}';
+        elem.style.color = '{font_color}';
+        elem.style.fontFamily = '{font_family}';
+    </script>
+    """
+    st.components.v1.html(html)
+
+label = "My text here"
+st.text_input(label)
+change_label_style(label, '30px')
+
+
+
+
+
+
+
+
+
 ##################################################
 with st.popover("OP"):
     st.markdown("Hello World 👋")
@@ -459,36 +484,3 @@ example1()
 
 ####################################################################################################
 # https://discuss.streamlit.io/t/anybody-interested-in-simple-component-to-float-containers/45013/5
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def change_label_style(label, font_size='12px', font_color='blue', font_family='sans-serif'):
-    html = f"""
-    <script>
-        var elems = window.parent.document.querySelectorAll('p');
-        var elem = Array.from(elems).find(x => x.innerText == '{label}');
-        elem.style.fontSize = '{font_size}';
-        elem.style.color = '{font_color}';
-        elem.style.fontFamily = '{font_family}';
-    </script>
-    """
-    st.components.v1.html(html)
-
-label = "My text here"
-st.text_input(label)
-change_label_style(label, '30px')
