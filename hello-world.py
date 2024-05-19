2250529ee40ac3e2b5a0b6b47073f1a7eb26b2d1
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-19 22:06:01 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -312,7 +312,7 @@ st.write("Your name:")
 def change_label_style(label, font_size='12px', font_color='blue', font_family='sans-serif'):
     html = f"""
     <script>
-        var elems = window.parent.document.querySelectorAll('p');
+        var elems = window.parent.document.querySelectorAll('a');
         var elem = Array.from(elems).find(x => x.innerText == '{label}');
         elem.style.fontSize = '{font_size}';
         elem.style.color = '{font_color}';
@@ -321,7 +321,7 @@ def change_label_style(label, font_size='12px', font_color='blue', font_family='
     """
     st.components.v1.html(html)
 
-label = "My text here"
+label = "324 My text here"
 st.text_input(label)
 change_label_style(label, '30px')
 
