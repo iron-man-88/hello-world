459ebc9ccd8e5acbf874a1ba6a322bd908d0edfe
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-19 20:49:36 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -459,3 +459,36 @@ example1()
 
 ####################################################################################################
 # https://discuss.streamlit.io/t/anybody-interested-in-simple-component-to-float-containers/45013/5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
+    html = f"""
+    <script>
+        var elems = window.parent.document.querySelectorAll('p');
+        var elem = Array.from(elems).find(x => x.innerText == '{label}');
+        elem.style.fontSize = '{font_size}';
+        elem.style.color = '{font_color}';
+        elem.style.fontFamily = '{font_family}';
+    </script>
+    """
+    st.components.v1.html(html)
+
+label = "My text here"
+st.text_input(label)
+change_label_style(label, '20px')
