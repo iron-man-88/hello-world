2775a53c66402dcc32e56dd65855f8c4dcd09637
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-21 22:47:48 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -343,6 +343,7 @@ div.div_inner:hover::after{{
 }}
 </style>
 <a data-tooltip="Kampf" class="ttip">{variable_output}
+<a data-tooltip="Kampf" class="ttip" p class="a">{variable_output}</p>
 """
 ###<p class="a">{variable_output}</p>
 ###<div data-tooltip="Kampf" class="div_inner" <div class="div_outer">{variable_output}</div>
