603a51e1654486db9c02a97285ebdf5226ce5f9a
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-22 19:48:04 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -548,3 +548,50 @@ st.write("""
     }
   </style>
   """, unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################
+
+
+
+
+
+
+
+
+
+
+def create_container_with_color(id, color="#E4F2EC"):
+    #stw(id)
+    # todo: instead of color you can send in any css
+    plh = st.container()
+    html_code = """<div id = 'my_div_outer'></div>"""
+    st.markdown(html_code, unsafe_allow_html=True)
+    with plh:
+        inner_html_code = """<div id = 'my_div_inner_%s'></div>""" % id
+        plh.markdown(inner_html_code, unsafe_allow_html=True)
+    ## applying style
+    chat_plh_style = """
+        <style>
+            div[data-testid='stVerticalBlock']:has(div#my_div_inner_%s):not(:has(div#my_div_outer)) {
+                background-color: %s;
+                border-radius: 10px;
+                padding: 10px;height:10px
+            };
+        </style>
+        """
+    chat_plh_style = chat_plh_style % (id, color)
+    st.markdown(chat_plh_style, unsafe_allow_html=True)
+    return plh
