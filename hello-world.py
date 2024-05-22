1ff5c3a1c5fa34a6a2561ec91eba751ce1a4daef
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-22 20:03:27 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -587,8 +587,8 @@ def create_container_with_color(id, color="#E4F2EC"):
         <style>
             div[data-testid='stVerticalBlock']:has(div#my_div_inner_%s):not(:has(div#my_div_outer)) {
                 background-color: %s;
-                border-radius: 10px 10px 10px 20px;
-                padding: 10px;height:10px
+                border-radius: 10px;
+                padding: 10px 10px 20px 10px;height:10px
             };
         </style>
         """
