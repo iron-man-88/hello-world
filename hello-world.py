842b06a2d85b36e5996d83d08c3de84b6bdccde1
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-25 22:01:46 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -646,7 +646,7 @@ def main():
     variable_output = bbb
 
     # Load the Jinja2 template
-    with open("template1.html", "r") as template_file:
+    with open("template.html", "r") as template_file:
         template_content = template_file.read()
         jinja_template = Template(template_content)
 
