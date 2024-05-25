e3e3da6d8c152dd3594bc5896330987d2b3d6b42
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-25 11:01:26 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -654,8 +654,7 @@ def main():
     rendered_html = jinja_template.render(title=app_title, items=items, variable_output = variable_output)
 
     # Display the HTML in Streamlit app
-    components.html(rendered_html, width=400, height=230, scrolling=False)
-    #components.html("""<link rel="stylesheet" href="css.css">""",
-    #                rendered_html, width=400, height=230, scrolling=False)
+    #components.html(rendered_html, width=400, height=230, scrolling=False)
+    components.html("""<link rel="stylesheet" href="/static/css.css">""", rendered_html, width=400, height=230, scrolling=False)
 
 main()
