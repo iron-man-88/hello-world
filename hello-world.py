1a7028bb6979ba4b7591804ffc962595e7e15ee7
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-26 19:35:32 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -329,7 +329,7 @@ position:relative;
  }}
 div.square{{
   background-color: lightblue;
-  min-width: 2px;
+  max-width: 2px;
   position:relative;
   left: {left_position}px;
   top: {top_position}px;
