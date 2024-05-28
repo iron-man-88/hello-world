2ca7f05f5848f9130bcb85f6cf09a5f8a70c226b
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-05-28 23:40:18 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -437,7 +437,7 @@ def create_container_with_color(id, color="#E4F2EC", left="0px"):
     ## applying style
     chat_plh_style = """
         <style>
-            div[data-testid='stVerticalBlock']:has(div#my_div_inner_%s) {
+            div{
                 background-color: %s;
                 border-radius: 10px;
                 padding: 10px 10px 20px 10px;height:10px
