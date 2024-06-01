bf6e64d6df1c4cc53fdde3c457eababf36047095
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-01 11:32:58 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -463,23 +463,14 @@ html_temp = """
 
 #htmll_code = """<div id='mmy_div_outer' style='background-color:{}; color:{}; position:relative; left:{}; top:{}; width:fit-content;
 #height:fit-content;'>aaa</div>"""
-htmll_code = """<div id='mmy_div_outer' style='background-color:{}; color:{}; position:relative; left:{}; top:{}; width:fit-content;
-             height:fit-content;'><a data-tooltip={div_tooltip} class="ttip">K&auml;mpfer
-             <p>My mother has <span style="color:blue">blue</span> eyes.</p>
-             <p>This is a simple <bigcolor>Streamlit</bigcolor> app with a Jinja2 template.</p>
-             <p>We're sorry, that <bigcolor><red>todo</red></bigcolor> item was not found:</p>
-             </div>"""
+#st.markdown(htmll_code.format(bgcolor, fontcolor, pos_left, pos_top),unsafe_allow_html=True)
 
 fhtmll_code = f"""<div id='mmy_div_outer' style='background-color:{bgcolor}; color:{fontcolor}; position:relative; left:{pos_left};
-             top:{pos_top}; width:fit-content;
-             height:fit-content;'><a data-tooltip="Kampf" class="ttip">{div_content}
-             </div>"""
+             top:{pos_top}; width:fit-content; height:fit-content;'><a data-tooltip="Kampf" class="ttip">{div_content}</div>"""
 
-#st.markdown(htmll_code.format(bgcolor, fontcolor, pos_left, pos_top),unsafe_allow_html=True)
 st.markdown(fhtmll_code,unsafe_allow_html=True)
 
-st.markdown("<div style='background-color:{}; color:{}; position:relative; left:10px; top:10px;'>Hello Streamlit</p></div>".format(bgcolor, fontcolor),unsafe_allow_html=True)
-
+###########################################################################
 #https://pytutorial.com/python-variable-in-string/#%%20Operator
 # Variables
 name = "Pytutorial"
