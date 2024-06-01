6ef1a787264b690c4ebeb05e9843459d76a9f459
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-01 22:49:41 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -459,7 +459,7 @@ div_content="""K&auml;mpfer<p>My mother has <span style="color:blue">blue</span>
 
 fhtmll_code = f"""<div id={page_no} style='background-color:{bgcolor}; color:{fontcolor}; position:relative;
               left:{globals()[page_no + "_" + str(var_num) + pos_left]}; top:{globals()[page_no + "_" + str(var_num) + pos_top]}; width:fit-content; height:fit-content;'>
-              <a data-tooltip=page_no class="ttip">{div_content}</div>"""
+              <a data-tooltip={page_no} class="ttip">{div_content}</div>"""
 st.markdown(fhtmll_code,unsafe_allow_html=True)
 #####
 html_temp = """
