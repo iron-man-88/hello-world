277c1598fe54c94162fe27fa34ecc5912aa9a0af
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-01 12:06:24 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -448,12 +448,12 @@ dynamic_1_variable = dynamic_1_variable +1
 st.write(dynamic_1_variable) # Output: 43
 #################################################################################################################
 #bgcolor=None; fontcolor=None; position = None; top = None; left = None
-page_no = "div_S001_"
+page_no = "div_S001"
 
 bgcolor = st.color_picker("Pick a Background color")
 fontcolor = st.color_picker("Pick a Font Color","#fff")
 pos_left= "100px"
-globals()[page_no + str(var_num) + pos_left] = "100px"   # eigentlich später Wert aus DB
+globals()[page_no + str("_+"var_num) + pos_left] = "100px"   # eigentlich später Wert aus DB
 pos_top="10px"
 div_tooltip="Kampf"
 div_content="""K&auml;mpfer<p>My mother has <span style="color:blue">blue</span> eyes.</p><p>This is a simple
