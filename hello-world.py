96abd6edd683f0df34c17c15ed92654c18331720
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-18 10:08:42 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -504,3 +504,28 @@ return_value = st_javascript(js_code)
 st.markdown(f"Return value was505: {return_value}")
 st.write(f"Return value was506: {return_value}")
 ###########################################################################
+
+
+
+
+
+
+
+###########################################################################
+
+import streamlit.components.v1 as components
+
+html_string = '''
+<h1>HTML string in RED</h1>
+
+<script language="javascript">
+  document.querySelector("h1").style.color = "red";
+  console.log("Streamlit runs JavaScript");
+  alert("Streamlit runs JavaScript");
+</script>
+'''
+
+components.html(html_string)  # JavaScript works
+
+st.markdown(html_string, unsafe_allow_html=True)  # JavaScript doesn't work
+###########################################################################
