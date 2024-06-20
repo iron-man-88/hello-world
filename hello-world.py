6590f2473b39e0e452fb2f3fca30ff832aac92b5
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-20 09:03:54 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -301,11 +301,11 @@ div_id = "my_div_1"
 
 html_str = f"""
 <style>div.square{{left: {left_position}px; top: {top_position}px;}}</style>
-<div <a href='#' id = {div_id}  class="square div-id" contenteditable="true">{variable_output}
+<a id = {div_id}  class="square div-id" contenteditable="true">{variable_output}
 <p>My mother has <span style="color:green">green</span> eyes.</p>
 <p>This is a simple <bigcolor>Streamlit</bigcolor> app with a Jinja2 template.</p>
 <p>We're sorry, that <bigcolor><red>todo</red></bigcolor> item was not found:</p>
-></div>
+></a>
 """
 #<div class="square"><a data-tooltip="Kampf" class="ttip">K&auml;mpfer + {variable_output}</div>
 #st.markdown(html_str, unsafe_allow_html=True, help='354 Select a number out of 3 choices')
