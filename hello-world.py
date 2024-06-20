ed779aa59245973d937f9db84d75019a346993d8
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-20 08:49:12 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -505,8 +505,8 @@ from st_click_detector import click_detector
 
 content = """<p><a href='#' id='Link 1'>First link</a></p>
     <p><a href='#' id='Link 2'>Second link</a></p>
-    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
-    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+    <a href='#' id='I-bin-Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='I-bin-Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
     """
 clicked = click_detector(content)
 
