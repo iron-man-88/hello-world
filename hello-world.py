29b91f79acc1e88abfc0faf8eccef8ae10542313
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-21 10:19:23 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -14,6 +14,7 @@ file = open("./database/neu.csv", "r")
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
+st.write(data)
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 st.markdown( # https://stackoverflow.com/questions/74611608/how-to-change-the-height-of-streamlit-sidebar
     """
