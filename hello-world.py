530e50ae5a92e83522e589f2f4a4700c61cdcc41
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-21 12:11:40 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -17,7 +17,8 @@ file.close()
 st.write(data)
 import json
 dates_json = json.dumps(data)
-dates_json_type = json.dumps(type(data))
+st.write(dates_json)
+dates_json = json.dumps(type(data))
 st.write(dates_json)
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
