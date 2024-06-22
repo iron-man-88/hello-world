4f8c6f2bff98e8573d43e3c4bf7547a4829f35cd
iron-man-88 <123287738+iron-man-88@users.noreply.github.com>
2024-06-22 08:11:07 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -16,7 +16,7 @@ data = list(csv.reader(file, delimiter=","))
 file.close()
 
 ## print(data)
-#st.write(data)
+st.write("19", data)
 import json
 dates_json = json.dumps(data)
 #st.write("22 " + dates_json)
